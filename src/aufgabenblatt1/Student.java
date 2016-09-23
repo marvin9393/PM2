f0d04307d84c6f2b9c557875cce66eac6057824c
marvin9393 <marvinpetersen@icloud.com>
2016-09-23 14:42:04 +0200

    Aufagbe in den Kommentaren angelegt

--- src/aufgabenblatt1/Student.java
+++ src/aufgabenblatt1/Student.java
@@ -9,7 +9,7 @@ package aufgabenblatt1;
 * Praktikum TIPM2, WS16-17
 * Gruppe: Marvin Petersen (marvin.petersen@haw-hamburg.de),
 * Sahin Tekes (sahin.tekes@haw-hamburg.de)
-* Aufgabe: Aufgabenblatt xx, Aufgabe xx
+* Aufgabe: Aufgabenblatt 1, Aufgabe 1.1
 * Verwendete Quellen: */
 public class Student {
 
