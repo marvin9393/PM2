40a87048ad1916bf5ebbeb9f81b2599b022d0725
marvin9393 <marvinpetersen@icloud.com>
2016-09-26 20:21:07 +0200

    Erstellen der Klasse Fachhochschule, HashCode und equals Implementiert, CompareTo implementiert. Fehlt noch die Sortierung

---
+++ src/aufgabenblatt1/Fachhochschule.java
@@ -0,0 +1,46 @@
+/**
+*
+*
+*
+*/
+package aufgabenblatt1;
+import java.util.*;
+/**
+* Praktikum TIPM2, WS16-17
+* Gruppe: Marvin Petersen (marvin.petersen@haw-hamburg.de),
+* Sahin Tekes (sahin.tekes@haw-hamburg.de)
+* Aufgabe: Aufgabenblatt 1, Aufgabe 1.1
+* Verwendete Quellen: */
+public class Fachhochschule {
+	
+	/**
+	 * Statische Objektvariable zählt die Studenten
+	 */
+	public static int anzahlStudenten=0;
+	
+	/**
+	 * Liste der Studenten an der Fachhochschule
+	 */
+	private List<Student> studenten;
+	
+	/**
+	 * Konstruktor dem man beliebig viele Studenten mitgeben kann.
+	 * @param student
+	 */
+	public Fachhochschule(Student...student){
+		studenten=new LinkedList<Student>();
+		for(int i=0;i<student.length;i++){
+			studenten.add(student[i]);
+		}
+	}
+	
+	/**
+	 * Ein Student in die Liste hinzufügen
+	 * @param student
+	 */
+	public void studentHinzufuegen(Student student){
+		studenten.add(student);
+	}
+	
+
+}
--- src/aufgabenblatt1/Student.java
+++ src/aufgabenblatt1/Student.java
@@ -7,7 +7,7 @@ import java.util.*;
 * Sahin Tekes (sahin.tekes@haw-hamburg.de)
 * Aufgabe: Aufgabenblatt 1, Aufgabe 1.1
 * Verwendete Quellen: */
-public class Student {
+public class Student implements Comparable<Student>{
 	
 	/**
 	 * Objektvariable die den Vornamen speichert.
@@ -22,11 +22,69 @@ public class Student {
 	/**
 	 * Objektvariable die die Matrikelnummer speichert.
 	 */
-	private int Matrikelnummer;
+	private int matrikelnummer;
+	
+	
 	
 	/**
 	 * Objektvariable die eine Liste der Prüfungen speichert.
 	 */
 	private List<Pruefungsleistung> pruefungsleistung;
 
+	/**
+	 * Konstruktor Initialisiert die Objektvariablen
+	 */
+	public Student(String vorname, String nachname){
+		this.vorname=vorname;
+		this.nachname=nachname;
+		Fachhochschule.anzahlStudenten++;
+		matrikelnummer=Fachhochschule.anzahlStudenten;
+		pruefungsleistung= new LinkedList<Pruefungsleistung>(); //lernen
+	}
+	
+	/**
+	 * Getter Vorname
+	 */
+	public String getVorname(){
+		return vorname;
+	}
+	
+	/**
+	 * Getter Nachname
+	 */
+	public String getNachname(){
+		return nachname;
+	}
+	
+	/**
+	 * Getter Matrikelnummer
+	 */
+	public int getMatrikelnummer(){
+		return matrikelnummer;
+	}
+	
+	@Override
+	public boolean equals(Object anderesObject){
+		if(!(anderesObject instanceof Student)){
+			return false;
+		}
+		Student andereStudent=(Student) anderesObject;
+		return (matrikelnummer==andereStudent.matrikelnummer);
+	}
+	
+	@Override
+	public int hashCode(){
+		return matrikelnummer;
+	}
+	
+	@Override
+	public int compareTo(Student andererStudent){
+		Integer student1=getMatrikelnummer();
+		Integer student2=andererStudent.getMatrikelnummer();
+		return student1.compareTo(student2);
+	}
+	
+	
+	
+	
 }
