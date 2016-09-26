0dacf31c2e6e5379985b847d4922e8374b8e8d95
marvin9393 <marvinpetersen@icloud.com>
2016-09-26 07:54:04 +0200

    bearbeitung der Klasse Pruefungsleistung

--- src/aufgabenblatt1/Pruefungsleistung.java
+++ src/aufgabenblatt1/Pruefungsleistung.java
@@ -9,8 +9,26 @@ package aufgabenblatt1;
 * Praktikum TIPM2, WS16-17
 * Gruppe: Marvin Petersen (marvin.petersen@haw-hamburg.de),
 * Sahin Tekes (sahin.tekes@haw-hamburg.de)
-* Aufgabe: Aufgabenblatt xx, Aufgabe xx
+* Aufgabe: Aufgabenblatt 1, Aufgabe 1.1
 * Verwendete Quellen: */
 public class Pruefungsleistung {
+	/**
+	 * Objektvariable die den name der Veranstaltung speichert
+	 */
+	private String nameDerVeranstaltung;
+	
+	/**
+	 * Objektvariable die die Note der Veranstaltung speichert.
+	 */
+	private int note;
+	
+	/**
+	 * Konstruktor
+	 */
+	public Pruefungsleistung(String nameDerVeranstaltung, int note){
+		this.nameDerVeranstaltung=nameDerVeranstaltung;
+		this.note=note;
+	}
+	
 
 }
