f08db3d913ef5f8e55d92280a45930fd5a03d65f
marvin9393 <marvinpetersen@icloud.com>
2016-09-26 07:49:01 +0200

    Aufgabe1 PDF eingearbeitet und erstellen der Pruefungsleistung

---
+++ src/aufgabenblatt1/Pruefungsleistung.java
@@ -0,0 +1,16 @@
+/**
+*
+*
+*
+*/
+package aufgabenblatt1;
+
+/**
+* Praktikum TIPM2, WS16-17
+* Gruppe: Marvin Petersen (marvin.petersen@haw-hamburg.de),
+* Sahin Tekes (sahin.tekes@haw-hamburg.de)
+* Aufgabe: Aufgabenblatt xx, Aufgabe xx
+* Verwendete Quellen: */
+public class Pruefungsleistung {
+
+}
