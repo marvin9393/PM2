95e7ad072c036f99835eb27bb594bf5f293f6226
marvin9393 <marvinpetersen@icloud.com>
2016-10-02 17:34:40 +0200

    Erzeugen der Testklasse und erster test

---
+++ src/aufgabenblatt1/AnwendungTesten.java
@@ -0,0 +1,42 @@
+/**
+*
+*
+*
+*/
+package aufgabenblatt1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+* Praktikum TIPM2, WS16-17
+* Gruppe: Marvin Petersen (marvin.petersen@haw-hamburg.de),
+* Sahin Tekes (sahin.tekes@haw-hamburg.de)
+* Aufgabe: Aufgabenblatt xx, Aufgabe xx
+* Verwendete Quellen: */
+public class AnwendungTesten {
+
+	@Test
+	public void testErzeugenderFachhochschule(){
+		Student s1=new Student("Marvin","Petersen");
+		Student s2=new Student("Marvin","Mustermann");
+		Student s3=new Student("Sahin","Tekes");
+		
+		Fachhochschule f1=new Fachhochschule(s1,s2,s3);
+		assertEquals("Fehler beim erzeugen und Hinzufuegen",Fachhochschule.anzahlStudenten,7);
+	}
+	@Test
+	public void testHinzufuegen() {
+		Student s1=new Student("Marvin","Petersen");
+		Student s2=new Student("Marvin","Mustermann");
+		Student s3=new Student("Sahin","Tekes");
+		
+		Fachhochschule f1=new Fachhochschule(s1,s2,s3);
+		Student s4=new Student("Appelin","Ahomas");
+		f1.studentHinzufuegen(s4);
+		assertEquals("Fehler beim Hinzufügen",Fachhochschule.anzahlStudenten,4);
+		
+	}
+
+}
