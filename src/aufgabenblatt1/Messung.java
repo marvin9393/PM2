b7370075055b34852e0bec45620bfbcde3de88f0
marvin9393 <marvinpetersen@icloud.com>
2016-10-06 10:28:47 +0200

    Erzeugen und fertig stellen der ODT und der XML

--- src/aufgabenblatt1/Messung.java
+++ src/aufgabenblatt1/Messung.java
@@ -13,5 +13,40 @@ package aufgabenblatt1;
 * Verwendete Quellen: */
 public class Messung {
 
+	/**
+	 * Speichert die Uhrzeit der Messung in einer Objektvariable
+	 */
+	private int zeit;
+	
+	/**
+	 * Speichert die Temperatur in Grad Celsius in einer Objektvariable.
+	 */
+	private int temperatur;
+	
+	/**
+	 * Konstrukor zum zuweisen der Objektvariablen.
+	 * @param zeit
+	 * @param temperatur
+	 */
+	public Messung(int zeit, int temperatur){
+		this.zeit=zeit;
+		this.temperatur=temperatur;
+	}
+	
+	/**
+	 * Getter fuer die Zeit
+	 * @return
+	 */
+	public int getZeit(){
+		return zeit;
+	}
+	
+
+	/**
+	 * Getter fuer die Temperatur
+	 */
+	public int getTemperatur(){
+		return temperatur;
+	}
 	
 }
--- src/aufgabenblatt1/Sensoren.java
+++ src/aufgabenblatt1/Sensoren.java
@@ -5,12 +5,34 @@
 */
 package aufgabenblatt1;
 
+import java.util.*;
 /**
 * Praktikum TIPM2, WS16-17
 * Gruppe: Marvin Petersen (marvin.petersen@haw-hamburg.de),
 * Sahin Tekes (sahin.tekes@haw-hamburg.de)
-* Aufgabe: Aufgabenblatt xx, Aufgabe xx
+* Aufgabe: Aufgabenblatt 1, Aufgabe 1.2
 * Verwendete Quellen: */
 public class Sensoren {
 
+	/**
+	 * Speichert den Ort an dem der Sensor steht in einer Objektvariable
+	 */
+	private String ort;
+	
+	/**
+	 * Speichert die Messungen in einer Liste
+	 */
+	private List<Messung> messungen;
+	
+	public Sensoren(String ort){
+		this.ort=ort;
+	}
+	
+	/**
+	 * Getter für den Ort
+	 * @return
+	 */
+	public String getOrt(){
+		return ort;
+	}
 }
