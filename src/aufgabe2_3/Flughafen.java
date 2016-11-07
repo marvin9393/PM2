654f49b3f5bd5e0398ad404fd156f3992fdd3058
marvin9393 <marvinpetersen@icloud.com>
2016-11-07 21:20:23 +0100

    Ueberarbeit alles

--- src/aufgabe2_3/Flughafen.java
+++ src/aufgabe2_3/Flughafen.java
@@ -1,67 +1,109 @@
+/**
+*
+*
+*
+*/
 package aufgabe2_3;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Flughafen extends Thread {
-
+/**
+* Praktikum TIPM2, WS16-17
+* Gruppe: Marvin Petersen (marvin.petersen@haw-hamburg.de),
+* Sahin Tekes (sahin.tekes@haw-hamburg.de)
+* Aufgabe: Aufgabenblatt xx, Aufgabe xx
+* Verwendete Quellen: */
+public class Flughafen extends Thread{
+	
 	private int anzahlFlugzeuge;
-	private List<Flugzeug> flugzeuge;
+	private List<Flugzeug>flugzeuge;
 	private int zeit;
-	private List<String> stringArray;
-	private boolean landebahnFrei = true;
-
-	public Flughafen(int anzahlFlugzeuge) {
-		this.anzahlFlugzeuge = anzahlFlugzeuge;
-		flugzeuge = new ArrayList<Flugzeug>();
-		stringArray = new ArrayList<String>();
-		zeit = 0;
-
-		for (int i = 0; i < 3; i++) {
-			erzeugeFlugezug();
+	
+	public Flughafen(int anzahlFlugzeuge){
+		this.anzahlFlugzeuge=anzahlFlugzeuge;
+		flugzeuge=new ArrayList<Flugzeug>();
+		
+	}
+	
+	
+	
+	public void landen(Flugzeug flugzeug){
+		flugzeug.istGelandet();
+	}
+	
+	public void run(){
+		for(int i=0;i<anzahlFlugzeuge;i++){
+			flugzeuge.add(erzeugeFlugzeug(this, zeit));
+			flugzeuge.get(i).start();
 		}
-	}
-
-	public void landen(Flugzeug flugzeug) {
-		flugzeug.setStatus();
-	}
-
-	private Flugzeug erzeugeFlugezug() {
-		String anzahl = " " + anzahlFlugzeuge + "";
-		Flugzeug flugzeug = new Flugzeug(anzahl, 5, zeit);
-		flugzeuge.add(0, flugzeug);
-		anzahlFlugzeuge--;
-		return flugzeug;
-	}
-
-	public synchronized void run() {
-		while (!flugzeuge.isEmpty()) {
-			zeit++;
-			System.out.println("Zeit: " + zeit);
-			for (int i = 0; i < flugzeuge.size(); i++) {
-				if (flugzeuge.get(i) != null) {
-					flugzeuge.get(i).run();
-					if (flugzeuge.get(i).isGelandet()) {
-						flugzeuge.remove(i);
-
-						if (anzahlFlugzeuge != 0) {
-							erzeugeFlugezug();
-
-						}
-					}
+		while(true){
+			System.out.println("Zeit: "+zeit);
+			for(int i=0;i<flugzeuge.size();i++){
+				System.out.println(flugzeuge.get(i).toString());
+				if(flugzeuge.get(i).isGelandet()){
+					flugzeuge.remove(i);
+					flugzeuge.add(i,erzeugeFlugzeug(this,zeit));
 				}
+				if(flugzeuge.get(i).getFlugzeit()>0){
+					flugzeuge.get(i).setZeit(zeit);
+				}
+				
 			}
 			try {
 				sleep(500);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
+			zeit++;
 		}
 	}
-
-	public static void main(String[] args) {
-		Flughafen flughafen = new Flughafen(10);
+	
+	public Flugzeug erzeugeFlugzeug(Flughafen flughafen, int Zeit){
+		return new Flugzeug(generiereID(),generiereFlugdauer(),this,zeit);
+	}
+	
+	
+	public static void main(String[] args){
+		Flughafen flughafen=new Flughafen(5);
 		flughafen.start();
 	}
+	
+	private String generiereID(){
+		int zahl=(int) (Math.random()*10);
+		switch(zahl){
+		case 1: return "Turkish Airlines 132";
+		case 2: return "TUI 32";
+		case 3: return "Planet Express 2";
+		case 4: return "Air Berlin 1111";
+		case 5: return "German Wings 6666";
+		case 6: return "Euro Wings 12345";
+		case 7: return "Turkish Airlines 631243789";
+		case 8: return "British Airlines 13204i";
+		case 9: return "Fly Emirates 12347";
+		case 10: return "HVV Airlines 132";
+		default: return "Lufthansa 2297";
+		
+		}
+	}
+	private int generiereFlugdauer(){
+		int zahl=(int) (Math.random()*10);
+		switch(zahl){
+		case 1: return 5;
+		case 2: return 10;
+		case 3: return 15;
+		case 4: return 20;
+		case 5: return 25;
+		case 6: return 30;
+		case 7: return 35;
+		case 8: return 40;
+		case 9: return 18;
+		case 10: return 9;
+		default: return 12;
+		
+		}
+	}
+	
+	
 
 }
--- src/aufgabe2_3/Flugzeug.java
+++ src/aufgabe2_3/Flugzeug.java
@@ -1,118 +1,65 @@
 package aufgabe2_3;
 
-import java.util.Formatter;
-
 /**
- * 
  * Praktikum TIPM2, WS16-17 Gruppe: Marvin Petersen
  * (marvin.petersen@haw-hamburg.de), Sahin Tekes (sahin.tekes@haw-hamburg.de)
- * Aufgabe: Aufgabenblatt 2, Aufgabe 2.3 Verwendete Quellen:
+ * Aufgabe: Aufgabenblatt xx, Aufgabe xx Verwendete Quellen:
  */
-
 public class Flugzeug extends Thread {
-
-	/**
-	 * Objektvariable die eine Referenz auf ein flughafen speichert.
-	 */
 	private Flughafen flughafen;
-
-	/**
-	 * Objektvariable die den Namen festlegt
-	 */
 	private String id;
-
-	/**
-	 * Objektvariable die die Flugdauer speichert
-	 */
-	private final int flugdauer;
-
-	/**
-	 * Objektvariable die die Zeit speichert
-	 */
-	private final int startzeit;
-
-	/**
-	 * speichert den Status
-	 */
+	private int flugdauer;
+	private int startzeit;
+	private int zeit;
 	private Status status;
 
-	/**
-	 * speichert die zeit die das flugzeug noch fliegen muss bis es den
-	 * flughafen erreicht.
-	 */
-	private int zeit;
-
-	/**
-	 * 
-	 * @param flughafen
-	 * @param id
-	 * @param flugdauer
-	 */
-	public Flugzeug(String id, int flugdauer, int startzeit) {
+	public Flugzeug(String id, int flugdauer, Flughafen flughafen, int zeit) {
 		this.id = id;
 		this.flugdauer = flugdauer;
-		this.startzeit = startzeit;
-		this.zeit = flugdauer;
+		this.flughafen = flughafen;
+		startzeit = zeit;
+		this.zeit = zeit;
 		status = Status.IM_FLUG;
 	}
 
-	public Flugzeug(Flughafen flughafen, String id, int flugdauer, int startzeit) {
-		this.flughafen = flughafen;
-		this.flughafen = flughafen;
-		this.id = id;
-		this.flugdauer = flugdauer;
-		this.startzeit = startzeit;
-		this.zeit = startzeit + flugdauer;
-		status = Status.IM_FLUG;
+	public int getFlugzeit() {
+		return flugdauer + startzeit - zeit;
 	}
 
-	/**
-	 * gibt uns zurueck ob das flugzeug gelandet ist.
-	 * 
-	 * @return
-	 */
-	public boolean isGelandet() {
-		return status == Status.GELANDET;
-	}
-
-	/**
-	 * setzt den Status auf die Landung.
-	 */
-	public void setStatus() {
-		if (status == Status.IM_FLUG && zeit <= 0) {
-			status = Status.IM_LANDEANFLUG;
-			System.out.println(toString());
+	public void run() {
+		while (!isGelandet()) {
+			istGelandet();
 			try {
-				sleep(1500);
-				status = Status.GELANDET;
-				System.out.println("-> Flugzeug gelandet: " + toString());
+				sleep(500);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 		}
+
+	}
+
+	public void istGelandet() {
+		if (zeit == flugdauer + startzeit) {
+			status = Status.IM_LANDEANFLUG;
+			try {
+				sleep(1500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			status = Status.GELANDET;
+
+		}
 	}
 
 	public String toString() {
-
-		return "Flugzeug" + id + "(" + status.toString() + "), Zeit bis Ziel: " + zeit;
+		return "Flugzeug" + id + "(" + status.toString() + ", Zeit bis Ziel: " + (startzeit + flugdauer - zeit);
 	}
 
 	public void setZeit(int zeit) {
 		this.zeit = zeit;
 	}
 
-	@Override
-	public void run() {
-		System.out.println(toString());
-		setZeit(zeit - 1);
+	public boolean isGelandet() {
+		return status == Status.GELANDET;
 	}
-
-	// public static void main(String[] args){
-	// Flugzeug flugzeug=new Flugzeug("Air Berlin 112", 20, 0);
-	// Flugzeug flugzeug1=new Flugzeug("Tui 1", 20, 0);
-	// flugzeug.start();
-	// flugzeug1.start();
-	//
-	// }
-
 }
