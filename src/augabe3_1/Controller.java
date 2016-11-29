aeced647f5624a15ae31b21eca7cd4d17ac6c76b
marvin9393 <marvinpetersen@icloud.com>
2016-11-29 17:29:28 +0100

    FXML hinzugefuegt

---
+++ src/augabe3_1/Controller.java
@@ -0,0 +1,30 @@
+/**
+*
+*
+*
+*/
+package augabe3_1;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+/**
+* Praktikum TIPM2, WS16-17
+* Gruppe: Marvin Petersen (marvin.petersen@haw-hamburg.de),
+* Sahin Tekes (sahin.tekes@haw-hamburg.de)
+* Aufgabe: Aufgabenblatt xx, Aufgabe xx
+* Verwendete Quellen: */
+public class Controller implements Initializable{
+
+	/* (non-Javadoc)
+	 * @see javafx.fxml.Initializable#initialize(java.net.URL, java.util.ResourceBundle)
+	 */
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/augabe3_1/Main.java
@@ -0,0 +1,40 @@
+/**
+*
+*
+*
+*/
+package augabe3_1;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.SplitPane;
+import javafx.scene.layout.BorderPane;
+import javafx.fxml.FXMLLoader;
+
+/**
+* Praktikum TIPM2, WS16-17
+* Gruppe: Marvin Petersen (marvin.petersen@haw-hamburg.de),
+* Sahin Tekes (sahin.tekes@haw-hamburg.de)
+* Aufgabe: Aufgabenblatt xx, Aufgabe xx
+* Verwendete Quellen: */
+public class Main extends Application{
+
+
+	  @Override
+	  public void start(Stage primaryStage) {
+	    try {
+	    BorderPane wurzel =
+	          (BorderPane)FXMLLoader.load(getClass().getResource("GuiBahnhof.fxml"));
+	      Scene scene = new Scene(wurzel, 400, 400);
+	      primaryStage.setScene(scene);
+	      primaryStage.show();
+	    } catch (Exception e) {
+	      e.printStackTrace();
+	    }
+	  }
+
+	  public static void main(String[] args) {
+	    launch(args);
+	  }
+}
